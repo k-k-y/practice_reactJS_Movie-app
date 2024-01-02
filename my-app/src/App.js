eb5b305fd33a9e9a468a4555c6f1e7252763b471
k-k-y <rnjsrldud2003@gmail.com>
2024-01-02 18:23:30 +0900

    useEffect: execute function conditionally - only once | when specific state changed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,36 @@
-import Button from './Button';
-import styles from './App.module.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+	const [counter, setCounter] = useState(0);
+	const [keyword, setKeyword] = useState('');
+
+	function onClick() {
+		setCounter((prev) => prev + 1);
+	}
+
+	function onChange(event) {
+		setKeyword(event.target.value);
+	}
+	useEffect(() => {
+		if (keyword !== '') {
+			console.log('I run when keyword changed');
+		}
+	}, [keyword]);
+
+	useEffect(() => {
+		console.log('I run only once');
+	}, []);
+
+	useEffect(() => {
+		if (counter !== 0) {
+			console.log('I run when counter changed');
+		}
+	}, [counter]);
 	return (
 		<div>
-			<h1 className={styles.title}>Welcome Back!</h1>
-			<Button text={'Click me'} />
+			<input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
+			<h1>count: {counter}</h1>
+			<button onClick={onClick}>Click me</button>
 		</div>
 	);
 }
